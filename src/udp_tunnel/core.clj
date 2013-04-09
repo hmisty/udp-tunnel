2c8c51de86d8550c23ea77edaf104aa1fc278556
Evan Liu <hmisty@gmail.com>
2013-04-09 23:09:38 +0800

    support -f config_file

--- src/udp_tunnel/core.clj
+++ src/udp_tunnel/core.clj
@@ -7,11 +7,30 @@
 (def load-config
   (comp read-string slurp))
 
+(defn arg-parse
+  [args]
+  (loop [opts {}
+         x (first args)
+         xs (rest args)]
+    (cond 
+      (nil? x) opts
+      (not= (.startsWith x "-")) opts
+      (empty? xs) (assoc opts x :true)
+      (.startsWith (first xs) "-") (recur (assoc opts x :true) (first xs) (rest xs))
+      :default (recur (assoc opts x (first xs)) (second xs) (drop 2 xs)))))
+
 (defn -main [& args]
-  (let [config (load-config CONFIG_FILE)]
+  "command line args:
+  -f CONFIG_FILE. config.clj as default if not specified.
+  -s. override the mode as tunnel server.
+  -c. override the mode as tunnel client.
+  if there's neither -s nor -c, the :mode in CONFIG_FILE decides."
+  (let [{config-file "-f" mode-server "-s" mode-client "-c"
+         :or {config-file CONFIG_FILE}} (arg-parse args)
+        config (load-config config-file)]
     (start-proxy
-      (case (first args)
-        (nil) config
-        ("-s") (assoc config :mode :tunnel-server)
-        ("-c") (assoc config :mode :tunnel-client)))))
+      (cond
+        (true? mode-server) (assoc config :mode :tunnel-server)
+        (true? mode-client) (assoc config :mode :tunnel-client)
+        :default config))))
 
