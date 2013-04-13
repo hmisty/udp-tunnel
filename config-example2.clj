f56040f138d6d8b6c1c6e4a6f76942d6bd2b697d
Evan Liu <hmisty@gmail.com>
2013-04-13 18:00:39 +0800

    rec timeout 600

--- config-example2.clj
+++ config-example2.clj
@@ -2,4 +2,4 @@
  :tunnel-server   ["1.2.3.4" 50001 "1.2.3.4" 50000]
  :server          ["8.8.8.8" 53 "127.0.0.1" 9999]
  :password        "h1m@i3s$t5y^"
- :timeout         60}
+ :timeout         600}
