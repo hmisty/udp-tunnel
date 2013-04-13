9858d083a35b9ccc6621c8994a567d61ead88ed6
Evan Liu <hmisty@gmail.com>
2013-04-13 18:01:36 +0800

    rec 60 timeout

--- config.clj
+++ config.clj
@@ -2,10 +2,10 @@
  :tunnel-server     ["127.0.0.1" 9000]
  :server            ["127.0.0.1" 10000]
  :password          "h1m@i3s$t5y^"
- :timeout           60}
+ :timeout           600}
 
 {:tunnel-client     ["127.0.0.1" 8000 "127.0.0.1" 8001]
  :tunnel-server     ["127.0.0.1" 9000 "127.0.0.1" 9001]
  :server            ["127.0.0.1" 10000 "127.0.0.1" 10001]
  :password          "h1m@i3s$t5y^"
- :timeout           60}
+ :timeout           600}
