f67024e8c2dc76841fc1ca61a63a49a7f9965f8d
Evan Liu <hmisty@gmail.com>
2013-04-13 10:53:58 +0800

    remove unessary keys

--- config.clj
+++ config.clj
@@ -2,13 +2,11 @@
 {:mode              :tunnel-client
  :tunnel-client     ["127.0.0.1" 53]
  :tunnel-server     ["1.2.3.4" 9999]
- :server            ["8.8.8.8" 53]
  :password          "h1m@i3s$t5y^"
  :timeout           60}
 
 ;; tunnel-client <-> tunnel-server 1.2.3.4:9999 <-> server 8.8.8.8:53
 #_{:mode              :tunnel-server
-   :tunnel-client     ["127.0.0.1" 53]
    :tunnel-server     ["1.2.3.4" 9999]
    :server            ["8.8.8.8" 53]
    :password          "h1m@i3s$t5y^"
